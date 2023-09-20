efc6104ca66603f9fb723554b37d6b9aa7ab2729
Anastasia Kurymshina <a.kurymshina@gmail.com>
2023-09-20 15:36:46 +0400

    add Test for fail bild

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -26,5 +26,14 @@ public class CashbackHackServiceTest {
 
         assertEquals(expected, actual);
     }
+    @Test
+    public void calculateBoundaryIfEquals1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual= service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
 
 }
